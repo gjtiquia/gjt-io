e93c626308b3aa4effd5095159312269ad446ce8
GJ Tiquia <gershom.tiquia@gmail.com>
2025-06-29 09:38:06 -0400

    fix source.sh not working

--- public/source.sh
+++ public/source.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-curl -fsSL gjt.io/bashrc -o tmpgjbashrc && source tmpgjbashrc && rm tmpgjbashrc
+print_message()
+{
+    echo "[gjt.io/source.sh] $1"
+}
+
+print_message "downloading from gjt.io/bashrc"
+
+curl -fsSL gjt.io/bashrc -o ~/.bashrc_gj 
+
+print_message "saved to ~/.bashrc_gj"
+
+print_message "run the following command"
+
+# the user needs to run this themselves or else it wont work
+echo
+echo "source ~/.bashrc_gj"
+echo
+
+
