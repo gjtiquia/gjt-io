b51882e86c1f68db75f5d8d506544257a23879ba
GJ Tiquia <gershom.tiquia@gmail.com>
2025-11-22 14:04:35 -0500

    delete everything

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Elysia } from "elysia";
-
-const app = new Elysia()
-    .get("/", ({ redirect }) => redirect("https://gjtiquia.com/?utm_source=gjt"))
-    .listen(process.env.PORT || 3000);
-
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
