e7a853d3f05b6ffdf45e9ffea9d7e10cd09888ca
GJ Tiquia <gershom.tiquia@gmail.com>
2025-11-22 01:18:20 -0500

    listen to env PORT; utm_source=gjt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia()
-    .get("/", ({ redirect }) => redirect("https://gjtiquia.com"))
-    .listen(3000);
+    .get("/", ({ redirect }) => redirect("https://gjtiquia.com/?utm_source=gjt"))
+    .listen(process.env.PORT || 3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
