80fd3b671d9a3400dcdceea4a8ba836ac73376bf
GJ Tiquia <gershom.tiquia@gmail.com>
2025-11-24 16:14:02 -0500

    added links to vim nvim tmux configs (and other improvements)

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,39 @@
 import { Hono } from 'hono'
-import { HonoOptions } from 'hono/hono-base'
 
 const app = new Hono({
-    // so that /repo and /repo/ will be treated as the same endpoint
+    // so that GET /repo and /repo/ will be treated as the same endpoint
     // docs: https://hono.dev/docs/api/hono#strict-mode
     strict: false
 })
 
     // gjtiquia.com
+
     .get('/', (c) => c.redirect("https://gjtiquia.com/?utm_source=gjt"))
     .get('/resume', (c) => c.redirect("https://gjtiquia.com/resume/?utm_source=gjt"))
 
     // github
+
     .get('/repo', (c) => c.redirect("https://github.com/gjtiquia?tab=repositories"))
-    .get('/repos', (c) => c.redirect("https://github.com/gjtiquia?tab=repositories"))
+    .get('/repos', (c) => c.redirect("/repo"))
+
     .get('/dotfiles', (c) => c.redirect("https://github.com/gjtiquia/dotfiles"))
+    .get('/dotfile', (c) => c.redirect("/dotfiles"))
+
+    .get('/vpsrice', (c) => c.redirect("https://github.com/gjtiquia/.vpsrice"))
+
+    .get('/vim', (c) => c.redirect("https://github.com/gjtiquia/.vim"))
+
+    .get('/nvim', (c) => c.redirect("https://github.com/gjtiquia/kickstart.nvim"))
+    .get('/neovim', (c) => c.redirect("/nvim"))
+
+    .get('/tmux', (c) => c.redirect("https://github.com/gjtiquia/.tmux"))
 
     // projects
-    .get('/txt', (c) => c.redirect("https://minitextarea.gjt.io/"))
-    .get('/textarea', (c) => c.redirect("https://minitextarea.gjt.io/"))
 
-    .get('/cp', (c) => c.redirect("https://ijustwannacopypaste.gjt.io/"))
+    .get('/txt', (c) => c.redirect("https://minitextarea.gjt.io/"))
+    .get('/textarea', (c) => c.redirect("/txt"))
+
+    .get('/cp', (c) => c.redirect("https://cp.gjt.io"))
     .get('/copypaste', (c) => c.redirect("https://ijustwannacopypaste.gjt.io/"))
 
     .get('/base64', (c) => c.redirect("https://base64anything.gjt.io/"))
